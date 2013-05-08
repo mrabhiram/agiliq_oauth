e291639f95845bcb992a75b8e159a77a34ab5ecb
mrabhiram <abhiram.ampabathina@gmail.com>
2013-05-07 19:04:20 -0700

    updated changes

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
 	<title>Agiliq Resume Upload via oAuth</title>
 </head>
 <body>
-<form action=" http://join.agiliq.com/api/resume/upload/?access_token=kJM9B2Gn0nPEMMT4E5PACMfps3Lh7jxi5wcbXZN0orXDUmdjvr">
+<form action=" http://join.agiliq.com/api/resume/upload/?access_token=m4puCbI8eRcwVaG7r7aKKSfk1XMF02lUmsepqIekgogHUr9CB2" method="POST" enctype="multipart/form-data">
 <label for="first_name">First Name</label>	
-<input type="text" id="first_name"><br>
+<input type="text" name="first_name" ><br>
 <label for="last_name">Last Name</label>	
-<input type="text" id="last_name"><br>
-<label for="projects_uri">Github URL</label>	
-<input type="text" id="projects_uri"><br>
+<input type="text" name="last_name"><br>
+<label for="projects_url">Github URL</label>	
+<input type="text" name="projects_url"><br>
 <label for="code_url">Source_code_url</label>	
-<input type="text" id="code_url"><br>
+<input type="text" name="code_url"><br>
 <label for="resume">Resume</label>	
-<input type="file" id="resume"><br>
+<input type="file" name="resume"><br>
 
 <input type="submit">
 </form>
